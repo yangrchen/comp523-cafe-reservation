7dc8c2f12aaf9e75ff8c1bcd74950e2796bcf06a
Mohammad Haveliwala <mohammad.haveliwala@gmail.com>
2021-10-15 02:13:43 -0400

    added table availabiltiy querying; created defaultdict class

--- cafe_reservation/lib/utils.dart
+++ cafe_reservation/lib/utils.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 
 class Utils {
@@ -14,4 +16,26 @@ class Utils {
     900: const Color.fromRGBO(153, 186, 221, 1),
   };
   static MaterialColor carolinaBlue = MaterialColor(0xFF99badd, color);
+}
+
+class Defaultdict<K, V> extends MapBase<K, V> {
+  final Map<K, V> _map = {};
+  final V Function() _ifAbsent;
+
+  Defaultdict(this._ifAbsent);
+
+  @override
+  V operator [](Object? key) => _map.putIfAbsent(key as K, _ifAbsent);
+
+  @override
+  void operator []=(K key, V value) => _map[key] = value;
+
+  @override
+  void clear() => _map.clear();
+
+  @override
+  Iterable<K> get keys => _map.keys;
+
+  @override
+  V? remove(Object? key) => _map.remove(key);
 }
