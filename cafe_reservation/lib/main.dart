fdf9f4bd140b583afd3de380978ac6298d5f7b60
Yang Chen <yyangc88@gmail.com>
2021-11-11 12:47:43 -0500

    Fix for Apple Silicon; significant reduction in iOS build times from firestore

--- cafe_reservation/lib/main.dart
+++ cafe_reservation/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:cafe_reservation/pages/cafe_admin.dart';
 import 'package:cafe_reservation/pages/home_page.dart';
+import 'package:cafe_reservation/pages/login_page.dart';
 import 'package:cafe_reservation/utils.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -19,7 +20,11 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[HomePage(), CafeAdmin()];
+  static const List<Widget> _widgetOptions = <Widget>[
+    LoginPage(),
+    HomePage(),
+    CafeAdmin()
+  ];
 
   void _onItemTapped(int index) {
     setState(() {
