b4424aee8299703bb6e1ef6771ff9c19adf5a5c6
Yang Chen <yyangc88@gmail.com>
2021-11-25 02:42:51 -0500

    Current rreservation tab view

--- cafe_reservation/lib/pages/current_reservation.dart
+++ cafe_reservation/lib/pages/current_reservation.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:cafe_reservation/database.dart';
-import 'package:cafe_reservation/models/cafe.dart';
 import 'package:cafe_reservation/models/reservation.dart';
 import 'package:cafe_reservation/models/user.dart';
 import 'package:cafe_reservation/widgets/cafe_info_template.dart';
@@ -14,6 +13,9 @@ class CurrentReservationPage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textStyle = {
+      'light': FontWeight.w200,
+    };
     User user = Provider.of<User>(context);
     final _reservationMemo =
         useMemoized(() => Database.readReservation(userid: user.uid));
@@ -21,32 +23,68 @@ class CurrentReservationPage extends HookWidget {
     return CafeInfoTemplate(
       children: <Widget>[
         Text(
-          _reservation.hasData
-              ? _reservation.data!.cafe.name
-              : 'No Reservation',
+          _reservation.data?.cafe.name ?? 'No Reservation',
           style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
         const SizedBox(height: 5),
         Text(
-          _reservation.hasData
-              ? _reservation.data!.cafe.address
-              : 'No address found.',
-          style: const TextStyle(
+          _reservation.data?.cafe.address ?? 'No address found.',
+          style: TextStyle(
             fontSize: 14,
-            fontWeight: FontWeight.w200,
+            fontWeight: textStyle['light'],
           ),
         ),
-        const SizedBox(height: 100),
-        _buildReservationInfo(context,
-            reservation: _reservation.data as Reservation),
+        if (_reservation.hasData) ...[
+          const SizedBox(height: 5.0),
+          Text(
+            '${_reservation.data!.startTime}:00 - ${_reservation.data!.endTime}:00',
+            style: TextStyle(fontWeight: textStyle['light']),
+          ),
+          const SizedBox(height: 5.0),
+          Text(
+            _reservation.data!.size == 1
+                ? '${_reservation.data!.size} Seat'
+                : '${_reservation.data!.size} Seats',
+            style: TextStyle(fontWeight: textStyle['light']),
+          ),
+        ],
+        const SizedBox(height: 120.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: IconButton(
+                onPressed: () {
+                  log('testing');
+                },
+                icon: const Icon(Icons.edit, color: Colors.white),
+                iconSize: 50.0,
+              ),
+              decoration: const BoxDecoration(
+                color: Color(0xFF5EC7F8),
+                borderRadius: BorderRadius.all(Radius.circular(15.0)),
+              ),
+            ),
+            const SizedBox(width: 120.0),
+            Container(
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    Icons.close,
+                    color: Color(0xFF5EC7F8),
+                  ),
+                  iconSize: 50.0,
+                ),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: const Color(0xFF5EC7F8),
+                    width: 2.0,
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(15.0)),
+                )),
+          ],
+        )
       ],
     );
-  }
-
-  Widget _buildReservationInfo(BuildContext context,
-      {required Reservation reservation}) {
-    return Column(children: <Widget>[
-      Text(reservation.toString()),
-    ]);
   }
 }
