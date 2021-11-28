e255111a5a3dc553e4328abeaff742efbb2bfbf1
Yang Chen <yyangc88@gmail.com>
2021-11-28 14:37:19 -0500

    Cleaning up code writing

--- cafe_reservation/lib/models/user.dart
+++ cafe_reservation/lib/models/user.dart
@@ -1,9 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
-
-import '../utils.dart';
-import 'table.dart';
-
 class User {
   late String uid;
   late String email;
@@ -11,5 +5,4 @@ class User {
   User.blank();
 
   User(this.uid, this.email);
-  
 }
