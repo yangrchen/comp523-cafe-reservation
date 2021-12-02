30d8660285cf51a65b9e64bc610885fcf584e919
Yang Chen <yyangc88@gmail.com>
2021-12-01 22:17:11 -0500

    Simple profile page with sign out

--- cafe_reservation/lib/pages/profile_page.dart
+++ cafe_reservation/lib/pages/profile_page.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cafe_reservation/models/user.dart' as U;
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends HookWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -11,11 +13,15 @@ class ProfilePage extends HookWidget {
 
   @override
   Widget build(BuildContext context) {
+    U.User user = Provider.of<U.User>(context);
     return Container(
+      alignment: Alignment.center,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
+          Text(user.email.toString(),
+              style:
+                  const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
           ElevatedButton(
             onPressed: () async {
               await _signOut();
